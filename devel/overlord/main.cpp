64f366595d7d0c71bcdb389bf5851f418f5e88c0
asakrana <>
2003-04-20 14:08:09 +0000

    new project started

---
+++ devel/overlord/main.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+                          main.cpp 
+                             -------------------
+    begin                : Sun Apr 20 17:22:03 IDT 2003
+    copyright            : (C) 2003 by Alex Dribin
+    email                : alexliza@netvision.net.il
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                                            *
+ *   This program is free software; you can redistribute it and/or   *
+ *  modify it under the terms of the BSD License.                       *
+ *                                                                                            *
+ ***************************************************************************/
+ 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <iostream.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  cout << "Hello, World!" << endl;
+
+  return EXIT_SUCCESS;
+}
